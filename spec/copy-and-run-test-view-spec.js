4f9e0049480611a77004cc9ab5f2c625afdc26f5
Tom Kadwill <tomkadwill@gmail.com>
2017-08-20 20:59:39 +0100

    Remove unused code

--- spec/copy-and-run-test-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import CopyAndRunTestView from '../lib/copy-and-run-test-view';
-
-describe('CopyAndRunTestView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
